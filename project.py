ea6c94cc2c55ac232764becce6290b9c1bf8642d
Andrii-devPython <aruabush@gmail.com>
2026-04-14 19:22:21 +0200

    Implement add_product and remove_product logic

--- project.py
+++ project.py
@@ -1,5 +1,31 @@
 def main():
-    print("Hello")
+    products = []
+    add_product(products, "Laptop", 2000)
+    print(products)
+
+def add_product(products, name, price):
+    product_id = len(products) + 1
+    
+    product = {
+        "id" : product_id,
+        "name" : name,
+        "price" : price,
+    }
+    
+    products.append(product)
+        
+def remove_product(products, product_id):
+    for product in products:
+        if product["id"] == product_id:
+            products.remove(product)
+            return True
+    return False
+
+def add_to_cart(products, cart, product_id):
+    pass
+
+def show_cart(cart):
+    pass
 
 if __name__ == "__main__":
     main()
